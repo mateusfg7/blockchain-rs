38b305b51c4490e4ca0738e7f7d4815569f06a8e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-03 20:25:42 -0300

    feat(blockchain): implement `new()` function for `Blockchain` struct

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -1,3 +1,5 @@
+use chrono::Utc;
+
 use super::block::Block;
 
 type Blocks = Vec<Block>;
@@ -7,4 +9,25 @@ pub struct Blockchain {
     pub genesis_block: Block,
     pub chain: Blocks,
     pub difficulty: usize,
+}
+
+impl Blockchain {
+    pub fn new(difficulty: usize) -> Self {
+        // First block in the chain
+        let genesis_block = Block {
+            index: 0,
+            timestamp: Utc::now().timestamp_millis() as u64,
+            proof_of_work: u64::default(),
+            previous_hash: String::default(),
+            hash: String::default(),
+        };
+
+        let chain: Blocks = vec![genesis_block.clone()];
+
+        Blockchain {
+            genesis_block,
+            chain,
+            difficulty,
+        }
+    }
 }
