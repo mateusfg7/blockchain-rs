5939b9b25849bbe8216a69022fc516bf11d8ec00
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-03 22:05:20 -0300

    feat(blockchain): implement `add_block()` function for `Blockchain` struct

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -4,7 +4,7 @@ use super::block::Block;
 
 type Blocks = Vec<Block>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Blockchain {
     pub genesis_block: Block,
     pub chain: Blocks,
@@ -14,13 +14,15 @@ pub struct Blockchain {
 impl Blockchain {
     pub fn new(difficulty: usize) -> Self {
         // First block in the chain
-        let genesis_block = Block {
+        let mut genesis_block = Block {
             index: 0,
             timestamp: Utc::now().timestamp_millis() as u64,
             proof_of_work: u64::default(),
             previous_hash: String::default(),
             hash: String::default(),
         };
+
+        genesis_block.hash = genesis_block.calculate_hash();
 
         let chain: Blocks = vec![genesis_block.clone()];
 
@@ -29,5 +31,22 @@ impl Blockchain {
             chain,
             difficulty,
         }
+    }
+
+    pub fn add_block(&mut self) {
+        let previous_hash = self.chain[self.chain.len() - 1].hash.clone();
+        let mut new_block = Block::new(self.chain.len() as u64, previous_hash);
+
+        new_block.mine(self.clone());
+
+        self.chain.push(new_block.clone());
+
+        println!("Index ----------- {}", new_block.index);
+        println!("Timestamp ------- {}", new_block.timestamp);
+        println!("Proof of work --- {}", new_block.proof_of_work);
+        println!("Previous hash --- {}", new_block.previous_hash);
+        println!("Hash ------------ {}", new_block.hash);
+
+        println!("");
     }
 }
