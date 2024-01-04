75b244bf44078ada7686fff1fff6424b498ce841
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-03 22:07:28 -0300

    feat: initial loop for add blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod models;
+use models::Blockchain;
+
 fn main() {
-    println!("Hello, world!");
+    let difficulty = 3;
+    let mut blockchain = Blockchain::new(difficulty);
+
+    let n_of_blocks = 3;
+    let mut curr_n = 0;
+
+    println!("");
+    loop {
+        if curr_n < n_of_blocks {
+            blockchain.add_block();
+            curr_n += 1;
+        } else {
+            break;
+        }
+    }
 }
